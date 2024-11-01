e942ad4b0506507ad9cf56b50fa1104c50e2b732
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-01 11:18:03 +0100

    frontend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/year")
+fetch("whatyearisit-backend-d8mbnpjlt-nicolasjouberts-projects.vercel.app")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
